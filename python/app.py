2ca45a681b4348be6e8088cfb300db2417553212
Jacob Adelman <jacobzadelman@gmail.com>
2022-09-21 18:19:02 -0400

    clean up files

--- python/app.py
+++ python/app.py
@@ -1,31 +1,18 @@
 
-
-# config = tf.ConfigProto()
-# config.gpu_options.allow_growth = True
-# session = tf.Session(config=config)
-# os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
-    # Read images (of size 255 x 255) from file.
-# im1 = tf.image.decode_image(tf.io.read_file('../outputali/0.png'))
-# im2 = tf.image.decode_image(tf.io.read_file('../outputamz/0.png'))
-    # tf.shape(im1)  # `img1.png` has 3 channels; shape is `(255, 255, 3)`
-    # tf.shape(im2)  # `img2.png` has 3 channels; shape is `(255, 255, 3)`
-    # # Add an outer batch for each image.
-    # im1 = tf.expand_dims(im1, axis=0)
-    # im2 = tf.expand_dims(im2, axis=0)
-    # # Compute SSIM over tf.uint8 Tensors.
-    # ssim1 = tf.image.ssim(im1, im2, max_val=255, filter_size=11,
-    #                         filter_sigma=1.5, k1=0.01, k2=0.03)
-
-    # # Compute SSIM over tf.float32 Tensors.
-    # im1 = tf.image.convert_image_dtype(im1, tf.float32)
-    # im2 = tf.image.convert_image_dtype(im2, tf.float32)
-    # ssim2 = tf.image.ssim(im1, im2, max_val=1.0, filter_size=11,
-    #                         filter_sigma=1.5, k1=0.01, k2=0.03)
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
 from tensorflow.keras.models import Model
 import numpy as np
 from PIL import Image
+import tensorflow as tf
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+import csv
+import os
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+  tf.config.experimental.set_memory_growth(gpu, True)
 
 
 class FeatureExtractor:
@@ -49,16 +36,58 @@ class FeatureExtractor:
         feature = self.model.predict(x)[0]
         return feature 
 
-arr =[]
-for x in range(1,60):
+
+
+arr = []
+arr2 =[]
+for x in range(1,20):
     img=Image.open('../outputali/' +str(x)+'.jpg')
     im=FeatureExtractor().extract(img)
     arr.append(im)
-    print(arr[0])
-    if(x==2):
+    # print(arr[0])
+    for m in range(1,60):
+        # if(x>=1):
+        img2=Image.open('../outputamz/' +str(m)+'.jpg')
+        im2=FeatureExtractor().extract(img2)
+        arr2.append(im2)
+
         z=arr[x-1]
-        y=arr[x]
+        y=arr2[m-1]
         # def eucledian_distance(x,y):
         eucl_dist = np.linalg.norm(z - y)
+
+        
+
+
+        # filepath = Path('../output/amazon.csv')  # CSV file to update.
+
+        # with open(filepath, 'r', newline='') as csv_file, \
+        #     NamedTemporaryFile('w', newline='', dir=filepath.parent, delete=False) as tmp_file:
+
+        #     reader = csv.reader(csv_file)
+        #     writer = csv.writer(tmp_file)
+
+        #     # Replace value in the first column of the first 5 rows.
+        #     for data_value in range(1, 20):
+        #         row = next(reader)
+        #         row[2] = data_value
+        #         writer.writerow(row)
+
+        #     writer.writerows(reader)  # Copy remaining rows of original file.
+
+        # # Replace original file with updated version.
+        # os.replace(tmp_file.name, filepath)
+        
+        print("----- EUCL DIST -----")
+        print(str(x)+".jpg"+ "  "+ str(m)+".jpg")
         print(eucl_dist)
+        print("----- EUCL DIST -----")
         # return eucl_dist
+
+# def append(fname, data):
+#     with open(fname) as f:
+#         reader = csv.reader(f)
+#         data = list(reader) + list(data)
+#     with open(fname, 'w') as f:
+#         writer = csv.writer(f)
+#         writer.writerows(data)
